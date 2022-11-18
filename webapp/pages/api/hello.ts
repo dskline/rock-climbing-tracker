707dae9c2017c858aa1462141a8bcca2c3d0379f
Spencer Kline <dskline1@gmail.com>
2022-11-18 17:14:23 -0500

    tweak: connect supabase to the hello rest api

--- webapp/pages/api/hello.ts
+++ webapp/pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { createClient } from '@supabase/supabase-js'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+  res: NextApiResponse
+  ) {
+  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_PUBLIC_KEY) {
+    throw new Error('Missing the .env.local file?');
+  }
+
+  // Create a single supabase client for interacting with your database
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_PUBLIC_KEY
+  );
+
+  const { data } = await supabase.from('test_view').select('*');
+
+  if (data) {
+    res.status(200).json(data)
+  }
 }
