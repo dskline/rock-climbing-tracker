b978b5b9a3c792c3a8861a5c128d9986bb889788
Spencer Kline <dskline1@gmail.com>
2022-11-22 14:37:56 -0500

    feature: display an HTML table of the data retrieved from the API

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+
+type SessionExercise = {
+  type: "PUSHUP";
+  sessions: {
+    start_time: string;
+  };
+  exercise_pushups: Array<{
+    reps: number;
+  }>;
+};
+
 export default function Home() {
+  const [sessionExercises, setSessionExercises] = useState<SessionExercise[]>();
+
+  useEffect(() => {
+    fetchSessionData();
+  }, []);
+
+  const fetchSessionData = async () => {
+    const response = await fetch("/api/sessions");
+    const json = await response.json();
+    setSessionExercises(json.data);
+  };
 
   return (
     <div>
@@ -8,7 +31,9 @@ export default function Home() {
       <input id="repsInput" placeholder="# reps" type="text" />
       <button
         onClick={() => {
-          const repsInput = document.getElementById("repsInput") as HTMLInputElement;
+          const repsInput = document.getElementById(
+            "repsInput"
+          ) as HTMLInputElement;
           if (repsInput.value) {
             alert("You did " + repsInput.value + " reps!");
           }
@@ -16,6 +41,33 @@ export default function Home() {
       >
         Submit
       </button>
+      <br />
+      <br />
+      <div>
+        {!sessionExercises && (
+          <div>
+            <p>Loading...</p>
+          </div>
+        )}
+        {sessionExercises && (
+          <table>
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Reps</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sessionExercises.map((sessionExercise) => (
+                <tr key={sessionExercise.sessions.start_time}>
+                  <td>{sessionExercise.sessions.start_time}</td>
+                  <td>{sessionExercise.exercise_pushups[0].reps}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 }
